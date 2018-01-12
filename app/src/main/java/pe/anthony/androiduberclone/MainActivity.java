aa5e7536e787a39fe1082d8862592dec0ad34f13
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2018-01-12 11:37:22 -0500

    se desarrollo un login y un register exitoso

--- app/src/main/java/pe/anthony/androiduberclone/MainActivity.java
+++ app/src/main/java/pe/anthony/androiduberclone/MainActivity.java
@@ -1,13 +1,205 @@
 package pe.anthony.androiduberclone;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import pe.anthony.androiduberclone.Model.User;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSignIn,btnRegister;
+    RelativeLayout rootLayout;  //este es el id del activity_main.xml
+//   Esto es firebase
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+//        Esto es de una libreria para cambiar el tipo de fuente de los text view
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//      Es necesario setear la nueva fuente antes de que construya la activity
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                            .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                            .setFontAttrId(R.attr.fontPath)
+                                            .build());
         setContentView(R.layout.activity_main);
+
+//      Inicializas firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+//      Inicializo los view del mainActivity
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnRegister = findViewById(R.id.btnRegister);
+        rootLayout = findViewById(R.id.rootLayout);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialog();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoginDialog();
+            }
+        });
+    }
+
+    private void showLoginDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.Signin);
+        dialog.setMessage("Porfavor necesitamos tu sign In");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View login_layout = inflater.inflate(R.layout.layout_login,null);
+
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.edtPassword);
+
+        dialog.setView(login_layout);
+        dialog.setPositiveButton(R.string.Signin, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                if(TextUtils.isEmpty(edtEmail.getText().toString())){
+                    Snackbar.make(rootLayout,"Please enter email address",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(edtPassword.getText().toString().length()<6){
+                    Snackbar.make(rootLayout,"Password too short",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+//                Login user
+               auth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString())
+                       .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                           @Override
+                           public void onSuccess(AuthResult authResult) {
+                               startActivity(new Intent(MainActivity.this,Welcome.class));
+                               finish();
+                           }
+                       })
+                       .addOnFailureListener(new OnFailureListener() {
+                           @Override
+                           public void onFailure(@NonNull Exception e) {
+                               Snackbar.make(rootLayout,"Failed"+e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                           }
+                       });
+            }
+        });
+        dialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void showRegisterDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(R.string.register);
+        dialog.setMessage("Porfavor necesitamos tu correo");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register,null);
+
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+        dialog.setView(register_layout);
+        dialog.setPositiveButton(R.string.register, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                if(TextUtils.isEmpty(edtEmail.getText().toString())){
+                    Snackbar.make(rootLayout,"Please enter email address",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(edtPassword.getText().toString().length()<6){
+                    Snackbar.make(rootLayout,"Password too short",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtName.getText().toString())){
+                    Snackbar.make(rootLayout,"Please enter name",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edtPhone.getText().toString())){
+                    Snackbar.make(rootLayout,"Please enter phone number",Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+//                Registrar new user
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+//                                Guarda al usuario para la BD
+                                User user = new User();
+                                user.setEmail(edtEmail.getText().toString());
+                                user.setName(edtName.getText().toString());
+                                user.setPassword(edtPassword.getText().toString());
+                                user.setPhone(edtPhone.getText().toString());
+//                                Usa email to key
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout,"Register success fully",Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout,"Failed"+e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout,"Failed"+e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+        dialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
     }
 }

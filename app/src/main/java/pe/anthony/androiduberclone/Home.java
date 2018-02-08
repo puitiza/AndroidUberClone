c811d267629ffb6d87f59cff7559138fb1ab03d4
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2018-02-07 19:51:05 -0500

    Se implementa el video 06 con un cambio. En vez de usar el LocationListener uso FusedLocationProviderClient mFusedLocationClient; private LocationCallback mLocationCallback;

--- app/src/main/java/pe/anthony/androiduberclone/Home.java
+++ app/src/main/java/pe/anthony/androiduberclone/Home.java
@@ -1,9 +1,18 @@
 package pe.anthony.androiduberclone;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,10 +22,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,14 +41,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener ,OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener ,OnMapReadyCallback
+                   ,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     SupportMapFragment mapFragment;
 
     //Location
+    private GoogleMap mMap;
     private static final int MY_PERMISSION_REQUEST_CODE = 7000;
     private static final int PLAY_SERVICE_RES_REQUEST = 7001;
 
@@ -46,12 +68,17 @@ public class Home extends AppCompatActivity
     DatabaseReference ref;
     GeoFire geoFire;
     Marker mUserMarker;
-    
+
+    //Esto es una forma mas reciente de implementar LocationListener solamente se necesita de estas variables
+    FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+
     //Aqui empieza todo lo que es para el Navigation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -65,6 +92,132 @@ public class Home extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
+                    mLastLocation = location;
+                    if (mUserMarker != null) {
+                        mUserMarker.remove();
+                    }
+
+                    //Place current location marker ... Al crear la activity se va a crear
+                   LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title("Your Location");
+//                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.car));
+                    mUserMarker = mMap.addMarker(markerOptions);
+//
+//                    //move map camera
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+
+                }
+            }
+        };
+
+        //        GeoFire
+        ref = FirebaseDatabase.getInstance().getReference("Riders");
+        geoFire = new GeoFire(ref);
+        setUpLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_CODE:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(checkPlayServices()){
+                        buildGoogleApiClient();
+                        createLocationRequest();
+                        displayLocation();
+                    }
+                }
+        }
+    }
+
+    private void setUpLocation() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+//              Request runtime permission
+            String[] Permissions ={android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, Permissions,MY_PERMISSION_REQUEST_CODE);
+        }else {
+            if(checkPlayServices()){
+                buildGoogleApiClient();
+                createLocationRequest(); //---estoy comentando esto porque ya esta definido en el onConnected
+                    displayLocation();
+            }
+        }
+    }
+
+    private void displayLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+
+//        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation !=null){
+                final double latitude = mLastLocation.getLatitude();
+                final double longitude = mLastLocation.getLongitude();
+
+//                Actualiza en firebase
+                geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+//                        Add Marker
+                        if(mUserMarker != null){
+                            mUserMarker.remove();//Remove already marker
+                            LatLng latLng = new LatLng(latitude,longitude);
+                            mUserMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(latLng)
+                                    .title("Your Location"));
+//                          Move camera to this positon
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15.0f));
+
+                        }
+                    }
+                });
+            }
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //This will return the finest location available.
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT); //Set the minimum displacement between location updates in meters
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        }
+    }
+
+    private void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    private boolean checkPlayServices() {
+//        Esta funcion es para compromar los play services del dispositivos
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int resulCode = googleAPI.isGooglePlayServicesAvailable(this);
+        if(resulCode != ConnectionResult.SUCCESS){
+            if(googleAPI.isUserResolvableError(resulCode)){
+                googleAPI.getErrorDialog(this, resulCode, PLAY_SERVICE_RES_REQUEST).show();
+            }
+            else{
+                Toast.makeText(this,"This device is not supported",Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -128,9 +281,23 @@ public class Home extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-        .position(new LatLng(37.7750,-122.4183))
-        .title("hello"));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.7750,-122.4183),12));
+        mMap = googleMap;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        /* displayLocation();
+        startLocationUpdates();*/
+        createLocationRequest();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }

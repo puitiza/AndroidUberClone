51af20fc0abea9c5320e46647d5a57c85fbd958d
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2018-02-09 16:53:06 -0500

    Se esta implementado el video 8 con una parte en la aplicacion RiderAPP, luego se tiene que cambiar en la aplicacion de DriverApp

--- app/src/main/java/pe/anthony/androiduberclone/Common/Common.java
+++ app/src/main/java/pe/anthony/androiduberclone/Common/Common.java
@@ -13,4 +13,9 @@ public class Common {
     public static IGoogleAPI getGoogleAPI(){
         return RetrofitClient.getClient(baseURL).create(IGoogleAPI.class);
     }
+
+    public static final String driver_tbl ="Drivers";
+    public static final String user_driver_tbl ="DriversInformation";
+    public static final String user_rider_tbl ="RidersInformation";
+    public static final String pickup_request_tbl ="PickupRequest";
 }
